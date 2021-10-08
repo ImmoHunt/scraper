cd15f6a4dea3c925330d7ff08c8f58e9b6d11dd8
ImmoHunt <35313883+ImmoHunt@users.noreply.github.com>
2021-10-08 14:42:46 +0200

    temos testes agora

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from . import views
 from django.conf.urls.static import static
-from ..scraper import settings
 
 urlpatterns = [
     path('', views.index ),
 ]
 
-urlpatterns += static(settings.STATIC_FRONT_URL, document_root=settings.STATIC_FRONT_ROOT)
+#urlpatterns += static(settings.STATIC_FRONT_URL, document_root=settings.STATIC_FRONT_ROOT)
