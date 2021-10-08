1caf1a2c916626d2293e16eeb25f6348f93d0dfd
ImmoHunt <35313883+ImmoHunt@users.noreply.github.com>
2021-10-08 15:07:10 +0200

    temos testes agora

--- static/frontend/main.js
+++ static/frontend/main.js
@@ -6,7 +6,7 @@
  * or disable the default devtool with "devtool: false".
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
-const AES = require('crypto-js/aes');
+//const AES = require('crypto-js/aes');
 const encryptUuid = AES.encrypt
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
