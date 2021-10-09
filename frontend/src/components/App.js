dec041f1c3a3854686ca8ab5c2b0375b2633751f
ImmoHunt <35313883+ImmoHunt@users.noreply.github.com>
2021-10-09 17:26:17 +0200

    temos testes agora

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,9 +1,9 @@
 import React, {useDebugValue, useState, Fragment} from 'react';
 
-const App = (props) => {
+const app = (props) => {
 	return (
 	<h1>Aqui nao se sabe bem que vai passar</h1>
 );
 }
 
-export default App;
+export default app;
