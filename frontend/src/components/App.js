12ff18176ad1ca1267eb94aec88d20df0fdbf5ac
ImmoHunt <35313883+ImmoHunt@users.noreply.github.com>
2021-10-14 21:27:57 +0200

    temos testes agora

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,50 +1,11 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [],
-      loaded: false,
-      placeholder: "Loading"
-    };
-  }
-
-  componentDidMount() {
-    fetch("api/lead")
-      .then(response => {
-        if (response.status > 400) {
-          return this.setState(() => {
-            return { placeholder: "Something went wrong!" };
-          });
-        }
-        return response.json();
-      })
-      .then(data => {
-        this.setState(() => {
-          return {
-            data,
-            loaded: true
-          };
-        });
-      });
-  }
-
-  render() {
+const App = () => {
     return (
-      <ul>
-        <li><h1>whazza</h1></li>
-        {this.state.data.map(contact => {
-          return (
-            <li key={contact.id}>
-              {contact.name} - {contact.email}
-            </li>
-          );
-        })}
-      </ul>
-    );
-  }
+        <div>
+        <h1>bem vindo</h1>
+        </div> );
 }
 
 export default App;
