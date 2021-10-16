6724c43982ad220cc8ce740e6ba76e15ca9e7a92
ImmoHunt <35313883+ImmoHunt@users.noreply.github.com>
2021-10-16 14:39:18 +0200

    temos testes agora

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,23 +2,12 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: [
-          // [style-loader](/loaders/style-loader)
-          { loader: "babel-loader" },
-          { loader: 'style-loader' },
-          // [css-loader](/loaders/css-loader)
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true
-            }
-          },
-          // [sass-loader](/loaders/sass-loader)
-          { loader: 'sass-loader' }
-        ]
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
       }
     ]
   }
 };
-Inli
