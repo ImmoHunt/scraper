217f014724da2159ef49edd7d9ea4d4077757c08
ImmoHunt <35313883+ImmoHunt@users.noreply.github.com>
2021-10-16 14:19:21 +0200

    temos testes agora

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,27 +1,13 @@
 module.exports = {
-  entry: path.resolve(__dirname, 'src', 'index.js'),
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
   module: {
     rules: [
       {
-        test: /\.(jsx|js)$/,
-        include: path.resolve(__dirname, 'src'),
+        test: /\.js$/,
         exclude: /node_modules/,
-        use: [{
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              ['@babel/preset-env', {
-                "targets": "defaults"
-              }],
-              '@babel/preset-react'
-            ]
-          }
-        }]
+        use: {
+          loader: "babel-loader"
+        }
       }
     ]
   }
-}
+};
