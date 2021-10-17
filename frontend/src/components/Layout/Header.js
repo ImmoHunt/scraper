4fdb50cd8b3a3fe6b3718de0bd4defabdbbf5a28
ImmoHunt <35313883+ImmoHunt@users.noreply.github.com>
2021-10-17 11:22:39 +0200

    temos testes agora

--- frontend/src/components/Layout/Header.js
+++ frontend/src/components/Layout/Header.js
@@ -1,6 +1,5 @@
 import React, { Fragment } from 'react';
 
-//import HeaderCartButton from './HeaderCartButton';
 import mealsImage from 'file-loader!../../assets/meals.jpg';
 import classes from 'style-loader!css-loader?modules!./Header.module.css';
 
@@ -12,7 +11,7 @@ const Header = (props) => {
         <header className={classes.header}>
             <h1>ReactMeals</h1>
             <button>Cart</button>
-            //<HeaderCartButton onClick={props.onShowCart} />
+
         </header>
         <div className={classes['main-image']}>
             <img src={mealsImage} alt='A mesa cheia da comida deliciosa!' />
