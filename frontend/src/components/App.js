dcb52485ec2c16af54abfb24326de4417121fe3d
ImmoHunt <35313883+ImmoHunt@users.noreply.github.com>
2021-11-04 13:45:57 +0100

    temos testes agora

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,8 +2,8 @@ import { Fragment, useState } from 'react';
 
 import Header from './Layout/Header';
 import Meals from './Meals/Meals';
-//import Cart from './Cart/Cart';
-//import CartProvider from '../store/CartProvider';
+import Cart from './Cart/Cart';
+import CartProvider from '../store/CartProvider';
 
 function App() {
   return (
