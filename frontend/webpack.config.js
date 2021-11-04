d73e850cf2090a6afe37c52e895f64e5781f0a0f
ImmoHunt <35313883+ImmoHunt@users.noreply.github.com>
2021-11-04 14:44:14 +0100

    temos testes agora

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  plugins: [
+    new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+        'process.env.MY_ENV': JSON.stringify(process.env.MY_ENV),
+    })
+  ],
   module: {
     rules: [
       {
