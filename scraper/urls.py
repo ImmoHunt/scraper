ca70e27e5e018d60db024fa9c49a08c59c926fd2
ImmoHunt <35313883+ImmoHunt@users.noreply.github.com>
2021-11-04 13:37:04 +0100

    temos testes agora

--- scraper/urls.py
+++ scraper/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from django.conf.urls.i18n import i18n_patterns
 
 
 urlpatterns = [
@@ -29,9 +28,3 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-urlpatterns += i18n_patterns(
-    path("js-settings/", "utils.views.render_js",
-    {"template_name": "settings.js"},
-    name="js_settings"),
-)
