604d6c7722b98a5d7248bb264ea078abbee9c473
ImmoHunt <35313883+ImmoHunt@users.noreply.github.com>
2021-11-04 15:12:21 +0100

    temos testes agora

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -6,23 +6,13 @@ import Cart from './Cart/Cart';
 import CartProvider from '../store/CartProvider';
 
 function App() {
-  const [cartIsShown, setCartIsShown] = useState(false);
-
-  const showCartHandler = () => {
-    setCartIsShown(true);
-  };
-
-  const hideCartHandler = () => {
-    setCartIsShown(false);
-    };
   return (
-    <CartProvider>
-      {cartIsShown && <Cart onClose={hideCartHandler} />}
-      <Header onShowCart={showCartHandler} />
-        <main>
-          <Meals />
-        </main>
-    </CartProvider>
+    <Fragment>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
   );
 }
 export default App;
