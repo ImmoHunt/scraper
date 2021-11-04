082749f2bfc512a9c90e59d1ce30434ca4e2a975
ImmoHunt <35313883+ImmoHunt@users.noreply.github.com>
2021-11-04 15:19:41 +0100

    temos testes agora

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,7 +2,7 @@ import { Fragment, useState } from 'react';
 
 import Header from './Layout/Header';
 import Meals from './Meals/Meals';
-
+import Cart from './Cart/Cart';
 
 function App() {
   return (
