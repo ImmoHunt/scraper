0df8f91ac0b8bd72503ac45c811995545b4499d2
ImmoHunt <35313883+ImmoHunt@users.noreply.github.com>
2021-11-04 15:07:34 +0100

    temos testes agora

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,13 +1,4 @@
-var webpack = require('webpack');
-process.env.MY_VARIABLE = 'kurwa';
-
 module.exports = {
-  plugins: [
-    new webpack.DefinePlugin({
-        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-        'process.env.MY_ENV': JSON.stringify(process.env.MY_ENV),
-    })
-  ],
   module: {
     rules: [
       {
