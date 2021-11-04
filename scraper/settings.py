90eb3a4c14af58db728307db2a364fcf513e3a36
ImmoHunt <35313883+ImmoHunt@users.noreply.github.com>
2021-11-04 13:07:22 +0100

    temos testes agora

--- scraper/settings.py
+++ scraper/settings.py
@@ -66,7 +66,9 @@ ROOT_URLCONF = 'scraper.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, "templates"),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -74,6 +76,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request',
+                'django.template.context_processors.i18n',
             ],
         },
     },
--- scraper/urls.py
+++ scraper/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 
 
 urlpatterns = [
@@ -28,3 +29,9 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += i18n_patterns(
+    path("js-settings/", "utils.views.render_js",
+    {"template_name": "settings.js"},
+    name="js_settings"),
+)
