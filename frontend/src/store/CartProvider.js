cf0ae7b5135b7cb133928974f45f5ba1bbac5685
ImmoHunt <35313883+ImmoHunt@users.noreply.github.com>
2021-11-14 18:05:24 +0100

    temos testes agora

--- frontend/src/store/CartProvider.js
+++ frontend/src/store/CartProvider.js
@@ -11,9 +11,11 @@ const cartReducer = (state, action) => {
     if (action.type === 'ADD') {
         const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
 
-        const existingCartItemIndex = state.items.findIndex(item => item.id === action.item.id);
+        const existingCartItemIndex = state.items.findIndex(
+        (item) => item.id === action.item.id
+        );
 
-        const existingCartItem = state.item[existingCartItemIndex];
+        const existingCartItem = state.items[existingCartItemIndex];
         let updatedItems;
 
         if (existingCartItem) {
@@ -30,7 +32,7 @@ const cartReducer = (state, action) => {
 
         return {
             items: updatedItems,
-            totalAmount: updatedTotalAmount
+            totalAmount: updatedTotalAmount,
         };
     }
     if ( action.type === 'REMOVE') {
@@ -66,6 +68,7 @@ const CartProvider = (props) => {
     };
 
     const cartContext = {
+
         items: cartState.items,
         totalAmount: cartState.totalAmount,
         addItem: addItemToCartHandler,
