963252b82cbb2eced675b053b9d8a2a7c0fa4eee
ImmoHunt <35313883+ImmoHunt@users.noreply.github.com>
2021-11-14 17:14:17 +0100

    temos testes agora

--- frontend/src/components/Meals/MealsSummary.js
+++ frontend/src/components/Meals/MealsSummary.js
@@ -1,7 +1,8 @@
 import classes from 'style-loader!css-loader?modules!./MealsSummary.module.css';
 
 const MealsSummary = () => {
-    return <section className={classes.summary}>
+    return (
+        <section className={classes.summary}>
         <h2>Delicious Food, Delivered to you</h2>
         <p>
             Choose your favourite meal from our broad selection of available meals and enjoy a delicious lunch or dinner at home.
@@ -10,6 +11,7 @@ const MealsSummary = () => {
             All our meals are cooked with high-quality ingredients, just-in-time and of course by experienced chefs!
         </p>
     </section>
+    );
 };
 
 export default MealsSummary;
